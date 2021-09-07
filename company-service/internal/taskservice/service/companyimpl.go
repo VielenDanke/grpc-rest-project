6915073208b6fbd632058e3ff6110091346a125f
Vladislav Dankevich <vielendanke1991@gmail.com>
2021-09-07 12:24:31 +0600

    feat:streams

--- company-service/internal/taskservice/service/companyimpl.go
+++ company-service/internal/taskservice/service/companyimpl.go
@@ -17,7 +17,7 @@ func NewTaskService(r repository.CompanyRepository) TaskService {
 }
 
 func (ts *TaskServiceImpl) CompanyByBin(ctx context.Context, req *pb.CompanyByBinRequest) (*pb.CompanyByBinResponse, error) {
-	log.Println(fmt.Sprintf("Company by IIN %s is requested", req.GetBin()))
+	log.Println(fmt.Sprintf("Company by Bin %s is requested", req.GetBin()))
 	name, err := ts.r.GetByBin(ctx, req.GetBin())
 	if err != nil {
 		return nil, err
